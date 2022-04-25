075d6465759b6e73447a95f87a4911c1fab0ba49
som <somsinghlodhi2323@gmail.com>
2022-04-25 20:19:29 +0530

    added new javascript which on click changes background color

--- js/my.js
+++ js/my.js
@@ -8,28 +8,38 @@
 // som.textContent = "bhagg madhar chod";
 // adding.append(som);
 // y.style.color = "white";
+$(function () {
+  alert("Click on Black Box");
+});
 let ctr = 0;
 let y = document.querySelector("button");
 y.addEventListener("click", function () {
   ctr++;
-  //   if (ctr == 1) {
-  //     y.style.position = "absolute";
-  //     y.style.top = "-30";
-  //     y.style.left = "50";
-  //   }
-  //   y.style.border = "2px solid rgb(255, 0, 247)";
-  y.style.border = "2px solid rgb(0, 255, 242)";
-  y.textContent = `you clicked me ${ctr} times 😎`;
-  let su = document.querySelector(".madh");
-  let new_element = document.createElement("h4");
-  new_element.textContent = `count: ${ctr}`;
-  su.appendChild(new_element);
+  y.style.margin = "50px auto 0";
   let me_j = document.querySelector("body");
   if (ctr == 100) {
     alert("Bass Kar Bhai 🤣😂");
   }
   me_j.style.background = "black";
   me_j.style.color = "white";
+  let r, g, b;
+  r = Math.floor(Math.random() * 255 + 1);
+  g = Math.floor(Math.random() * 255 + 1);
+  b = Math.floor(Math.random() * 255 + 1);
+  let a = Math.random();
+  me_j.style.background = `rgba(${r},${g},${b},${a})`;
+  //   if (ctr == 1) {
+  //     y.style.position = "absolute";
+  //     y.style.top = "-30";
+  //     y.style.left = "50";
+  //   }
+  //   y.style.border = "2px solid rgb(255, 0, 247)";
+  // y.style.border = "2px solid rgb(0, 255, 242)";
+  y.textContent = `Current color : rgb(${r},${g},${b}) `;
+  let su = document.querySelector(".madh");
+  let new_element = document.createElement("h4");
+  new_element.textContent = `count: ${ctr}`;
+  su.appendChild(new_element);
 });
 
 y.addEventListener("mouseover", function () {
